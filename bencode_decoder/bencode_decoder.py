cf68b4dc6952ad5250a54aa87b0c2208c57d77d2
Vancha <tjipkevanderheide90@gmail.com>
2024-06-10 11:09:18 +0200

    Update bencode_decoder.py

--- bencode_decoder/bencode_decoder.py
+++ bencode_decoder/bencode_decoder.py
@@ -20,7 +20,6 @@ class bdecoder:
         #byte strings are suffixed with a :, skip over that before returning
         next(byte_iterator)
         result = []
-        #tp = type(byte_iterator.peek())
         for _ in range(int(length)):
             result.append(next(byte_iterator))
         return bytes(bytearray(result))
@@ -80,9 +79,9 @@ class bdecoder:
         #skip over the initial 'd'
         next(byte_iterator)
 
-        result  = {}
+        result  = OrderedDict()
         while not chr(byte_iterator.peek()) == "e":
-            field_name =  bdecoder.decode_byte_string(byte_iterator)#.decode('utf_16_be')
+            field_name =  bdecoder.decode_byte_string(byte_iterator)
             field_value = bdecoder.decode_next(byte_iterator)
             result[field_name] = field_value
 
@@ -94,10 +93,60 @@ class bdecoder:
     Takes the torrent file, and attempts to parse it.
     returns the parsed value
     '''
-    def decode(torrent_file):
+    def decode_from_file(torrent_file):
         file  = open(torrent_file, "rb").read()
-        byte_iterator  = peekable(file)
+        return bdecoder.decode(file)
+        #byte_iterator  = peekable(file)
+        #return bdecoder.decode_next(byte_iterator)
+
+    def decode(byte_object):
+        byte_iterator = peekable(byte_object)
         return bdecoder.decode_next(byte_iterator)
 
+    '''
+    Encodes an object:
+    '''
+    def encode(obj):
+        return bdecoder.encode_next(obj)
+    '''
+    Encodes object types recursively
+    '''
+    def encode_next(obj):
+        #print(f'received object of type {type(obj)}')
+        t = type(obj)
+        if t in [dict, OrderedDict]:
+            return bdecoder.encode_dict(obj)
+        elif t == bytes:
+            return bdecoder.encode_byte_string(obj)
+        elif t == list:
+            return bdecoder.encode_list(obj)
+        elif t == int:
+            return bdecoder.encode_int(obj)
+        else:
+            raise Error('error, wrong type')
+
+    def encode_int(obj):
+        val = b"i" +  bytes(str(obj),'utf-8') + b"e"
+        return val
+        
+    def encode_list(obj):
+        encoded_list = bytearray(b"l")
+        for element in obj:
+            encoded_list += bdecoder.encode_next(element)
+        encoded_list += b"e"
+        return encoded_list
+
+    def encode_byte_string(key):
+        rawlen = len(key)
+        return str(rawlen).encode('utf-8') + b":" +  key
+
+    def encode_dict(obj):
+        encoded_dict = bytearray(b"d")
+        for key in obj.keys():
+            encoded_dict += bdecoder.encode_next(key)
+            encoded_dict += bdecoder.encode_next(obj[key])
+        encoded_dict += b"e"
+        return encoded_dict
+        
 #decoded_file = bdecoder.decode("./ubuntu.torrent")
 #print(decoded_file[b"info"])
